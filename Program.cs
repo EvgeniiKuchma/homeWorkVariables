ff109cf8ccf43ae19f66a5c0e4d313c3f91cee25
Евгений Кучма <desferto87@gmail.com>
2024-12-07 15:18:43 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homeWorkVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int refrigeratorTemperature = -10;
+            uint youAge = 10; 
+            double moneyIsAvailable = 0.1;
+            float sizeBroads = 4.3444f;
+            string youName = "Jack";
+            byte timeToRefand = 255;
+            short clothingSize = 127;
+            long speedOfLigth = 3000000000;
+            bool IsAvailableWallet = Convert.ToBoolean(1);
+            bool IsReadyToCook = false;
+            char letterOfAlphabet = 'A';
+        }
+    }
+}
